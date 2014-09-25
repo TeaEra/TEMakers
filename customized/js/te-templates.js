52275d5df87e2c18690576646221abfdde212460
CHEN Zhengong <cllf08214@qq.com>
2014-09-25 19:10:32 +0800

    Encapsulate all templates into window.TETemplates;

--- customized/js/te-templates.js
+++ customized/js/te-templates.js
@@ -1,114 +1,117 @@
 /**
  * Created by chenzhengtong on 2014/9/25.
  */
+(function () {
 
-"use strict";
+    "use strict";
 
-var templateProductCarousel = _.template('\
-<!-- -->\
-<div class="container" id="product-carousel">\
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">\
-        <!-- Indicators -->\
-        <ol class="carousel-indicators">\
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>\
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>\
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>\
-        </ol>\
-        <!-- Wrapper for slides -->\
-        <div class="carousel-inner" role="listbox">\
-            <div class="item active">\
-                <img src="customized/img/first-slide.svg" alt="...">\
-                <div class="carousel-caption">\
-                ...\
-                </div>\
-            </div>\
-            <div class="item">\
-                <img src="customized/img/second-slide.svg" alt="...">\
-                <div class="carousel-caption">\
-                ...\
-                </div>\
-            </div>\
-            <div class="item">\
-                <img src="customized/img/third-slide.svg" alt="...">\
-                <div class="carousel-caption">\
+    window.TETemplates = window.TETemplates || {};
+
+    window.TETemplates.templateProductCarousel = _.template('\
+    <!-- -->\
+    <div class="container" id="product-carousel">\
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">\
+            <!-- Indicators -->\
+            <ol class="carousel-indicators">\
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>\
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>\
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>\
+            </ol>\
+            <!-- Wrapper for slides -->\
+            <div class="carousel-inner" role="listbox">\
+                <div class="item active">\
+                    <img src="customized/img/first-slide.svg" alt="...">\
+                    <div class="carousel-caption">\
                     ...\
+                    </div>\
+                </div>\
+                <div class="item">\
+                    <img src="customized/img/second-slide.svg" alt="...">\
+                    <div class="carousel-caption">\
+                    ...\
+                    </div>\
+                </div>\
+                <div class="item">\
+                    <img src="customized/img/third-slide.svg" alt="...">\
+                    <div class="carousel-caption">\
+                        ...\
+                    </div>\
+                </div>\
+            </div>\
+            <!-- Controls -->\
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">\
+                <span class="glyphicon glyphicon-chevron-left"></span>\
+                <span class="sr-only">Previous</span>\
+            </a>\
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">\
+                <span class="glyphicon glyphicon-chevron-right"></span>\
+                <span class="sr-only">Next</span>\
+            </a>\
+        </div>\
+    </div>\
+    ');
+
+    window.TETemplates.templateProductThreeColumns = _.template('\
+    <!-- -->\
+    <div class="container" id="product-three-columns">\
+        <div class="col-md-4">\
+            <div class="panel panel-default">\
+                <div class="panel-body">\
+                    <img class="img-responsive" src="customized/img/Chrysanthemum.jpg" />\
+                </div>\
+                <div class="panel-footer">\
+                    <h6>Title</h6>\
+                    <p style="word-wrap: break-word;">\
+                        <small>Author: xxxxxx</small>\
+                    </p>\
+                    <p style="word-wrap: break-word;">\
+                        <small>\
+                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
+                        </small>\
+                    </p>\
                 </div>\
             </div>\
         </div>\
-        <!-- Controls -->\
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">\
-            <span class="glyphicon glyphicon-chevron-left"></span>\
-            <span class="sr-only">Previous</span>\
-        </a>\
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">\
-            <span class="glyphicon glyphicon-chevron-right"></span>\
-            <span class="sr-only">Next</span>\
-        </a>\
+        <div class="col-md-4">\
+            <div class="panel panel-default">\
+                <div class="panel-body">\
+                    <img class="img-responsive" src="customized/img/Desert.jpg" />\
+                </div>\
+                <div class="panel-footer">\
+                    <h6>Title</h6>\
+                    <p style="word-wrap: break-word;">\
+                        <small>Author: xxxxxx</small>\
+                    </p>\
+                    <p style="word-wrap: break-word;">\
+                        <small>\
+                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
+                        </small>\
+                    </p>\
+                </div>\
+            </div>\
+            </div>\
+        <div class="col-md-4">\
+            <div class="panel panel-default">\
+                <div class="panel-body">\
+                    <img class="img-responsive" src="customized/img/Hydrangeas.jpg" />\
+                </div>\
+                <div class="panel-footer">\
+                    <h6>Title</h6>\
+                    <p style="word-wrap: break-word;">\
+                        <small>Author: xxxxxx</small>\
+                    </p>\
+                    <p style="word-wrap: break-word;">\
+                        <small>\
+                        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
+                        </small>\
+                    </p>\
+                </div>\
+            </div>\
+        </div>\
     </div>\
-</div>\
-');
+    ');
 
-var templateProductThreeColumns = _.template('\
-<!-- -->\
-<div class="container" id="product-three-columns">\
-    <div class="col-md-4">\
-        <div class="panel panel-default">\
-            <div class="panel-body">\
-                <img class="img-responsive" src="customized/img/Chrysanthemum.jpg" />\
-            </div>\
-            <div class="panel-footer">\
-                <h6>Title</h6>\
-                <p style="word-wrap: break-word;">\
-                    <small>Author: xxxxxx</small>\
-                </p>\
-                <p style="word-wrap: break-word;">\
-                    <small>\
-                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
-                    </small>\
-                </p>\
-            </div>\
-        </div>\
-    </div>\
-    <div class="col-md-4">\
-        <div class="panel panel-default">\
-            <div class="panel-body">\
-                <img class="img-responsive" src="customized/img/Desert.jpg" />\
-            </div>\
-            <div class="panel-footer">\
-                <h6>Title</h6>\
-                <p style="word-wrap: break-word;">\
-                    <small>Author: xxxxxx</small>\
-                </p>\
-                <p style="word-wrap: break-word;">\
-                    <small>\
-                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
-                    </small>\
-                </p>\
-            </div>\
-        </div>\
-        </div>\
-    <div class="col-md-4">\
-        <div class="panel panel-default">\
-            <div class="panel-body">\
-                <img class="img-responsive" src="customized/img/Hydrangeas.jpg" />\
-            </div>\
-            <div class="panel-footer">\
-                <h6>Title</h6>\
-                <p style="word-wrap: break-word;">\
-                    <small>Author: xxxxxx</small>\
-                </p>\
-                <p style="word-wrap: break-word;">\
-                    <small>\
-                    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\
-                    </small>\
-                </p>\
-            </div>\
-        </div>\
-    </div>\
-</div>\
-');
-
-var templateLogin = _.template('\
+    window.TETemplates.templateLogin = _.template('\
     <!-- -->\
     <div class="container" id="form-login">\
         <div class="row">\
@@ -139,9 +142,9 @@ var templateLogin = _.template('\
             </div>\
         </div>\
     </div>\
-');
+    ');
 
-var templateRegister = _.template('\
+    window.TETemplates.templateRegister = _.template('\
     <!-- -->\
     <div class="container" id="form-register">\
         <div class="row">\
@@ -173,4 +176,6 @@ var templateRegister = _.template('\
             </div>\
         </div>\
     </div>\
-');
+    ');
+
+})();
