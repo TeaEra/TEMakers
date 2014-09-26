8dc449d1380d1688e35f08b66f904406585b4e99
CHEN Zhengong <cllf08214@qq.com>
2014-09-26 13:31:44 +0800

    Add codes for other buttons; Define variable in scss file;

--- customized/js/controller.js
+++ customized/js/controller.js
@@ -8,12 +8,36 @@
 
     window.controller = window.controller || {};
 
+    window.controller.showHome = function () {
+        $("#main-content").html(
+                TETemplates.templateProductCarousel()
+                + "<br/>"
+                + TETemplates.templateProductThreeColumns()
+        );
+    }
+
     window.controller.showLogin = function () {
         $("#main-content").html(TETemplates.templateLogin());
     };
 
     window.controller.showRegister = function () {
         $("#main-content").html(TETemplates.templateRegister());
+    };
+
+    window.controller.showPreSale = function () {
+        //
+    };
+
+    window.controller.showShop = function () {
+        //
+    };
+
+    window.controller.showVote = function () {
+        //
+    };
+
+    window.controller.showBBS = function () {
+        //
     };
 
     window.controller.activateCurrentButton = function (currObj) {
@@ -30,6 +54,21 @@
         }
         else if (currId === "btn-register") {
             window.controller.showRegister();
+        }
+        else if (currId === "btn-home") {
+            window.controller.showHome();
+        }
+        else if (currId === "btn-pre-sale") {
+            window.controller.showPreSale();
+        }
+        else if (currId === "btn-shop") {
+            window.controller.showShop();
+        }
+        else if (currId === "btn-vote") {
+            window.controller.showVote();
+        }
+        else if (currId === "btn-bbs") {
+            window.controller.showBBS();
         }
     };
 })();
